307dadc11c36e6c681653fb1a0f8428bb2db85ec
CST-11 <dicksonchoi888@gmail.com>
2022-07-24 22:50:45 +0800

    consolidate into one function(if keyword in i.text)

--- news_scraping_by_selenium.py
+++ news_scraping_by_selenium.py
@@ -6,60 +6,29 @@ from selenium.webdriver.support import expected_conditions as EC
 
 keyword= input("Please enter keyword:")
 
-def search_by_direct_XPATH(myList_1):
+
+def search_by_XPATH(myList_1):
     for k in range(len(myList_1)):
         driver = webdriver.Chrome(r"C:\Users\user\PycharmProjects\web_scraping\chromedriver.exe")
         driver.get(myList_1[k]['url'])
         driver.implicitly_wait(15)
         elem = driver.find_elements(By.XPATH, myList_1[k]['XPATH'])
-        links_and_titles = []
-        for i in elem:
-            links_and_titles.append(i.text + " " + i.get_attribute('href'))
-        match = [s for s in links_and_titles if keyword in s]
         print(myList_1[k]['media'])
-        print(match)
+        for i in elem:
+            if keyword in i.text:
+                print(i.text)
+                print(i.get_attribute("href"))
         print('')
         driver.quit()
 
-myList_1 = [{"media": "Cable","url":"https://www.i-cable.com/category/%E6%96%B0%E8%81%9E%E8%B3%87%E8%A8%8A/%E6%B8%AF%E8%81%9E/", "XPATH":"//h4[@class='post-title']/a"},
+myList_1 = [{"media": "商台","url":"https://www.881903.com/news/local", "XPATH":"//a[@class='news-grid__headline']"},
+            {"media": "TVB","url":"https://news.tvb.com/tc/local", "XPATH":"//a[.//h3]"},
+            {"media": "Cable TV","url":"https://www.i-cable.com/category/%E6%96%B0%E8%81%9E%E8%B3%87%E8%A8%8A/%E6%B8%AF%E8%81%9E/", "XPATH":"//h4[@class='post-title']/a"},
+            {"media": "Now TV","url":"https://news.now.com/home/local", "XPATH":"//a[@class='newsWrap clearfix']"},
             {"media": "文匯網","url":"https://www.wenweipo.com/immed/hongkong", "XPATH":"//div[@class='story-item-title text-overflow']/a"},
             {"media": "點新聞","url":"https://www.dotdotnews.com/immed/hknews", "XPATH":"//div[@class='text']/h2/a"}]
 
-search_by_direct_XPATH(myList_1)
+#myList_3 = [{"media": "東方","url":"https://hk.on.cc/hk/news/index.html", "XPATH":"//div/h1/a"}]
 
-def search_by_two_XPATH(myList_2):
-    for k in range(len(myList_2)):
-        driver = webdriver.Chrome(r"C:\Users\user\PycharmProjects\web_scraping\chromedriver.exe")
-        driver.get(myList_2[k]['url'])
-        driver.implicitly_wait(15)
-        elem = driver.find_elements(By.XPATH, myList_2[k]["XPATH_LINK"]) + driver.find_elements(By.XPATH, myList_2[k]["XPATH_TITLE"])
-        links_and_titles  = []
-        for i in elem:
-            links_and_titles.append(i.text)
-            links_and_titles.append(str(i.get_attribute('href')))
-        matches = [s for s in links_and_titles if keyword in s]
-        print(myList_2[k]["media"])
-        for match in matches:
-            index_of_match = links_and_titles.index(match)
-            index_of_match_link = index_of_match + 1
-            print(match)
-            print(links_and_titles[index_of_match_link])
-        print('')
-    driver.quit()
+search_by_XPATH(myList_1)
 
-myList_2 = [{"media": "商台即時","url":"https://www.881903.com/news/local", "XPATH_LINK":"//a[@class='news-grid__headline']", "XPATH_TITLE":"//a[@class='news-grid__headline']/span"},
-            {"media": "NowTV","url":"https://news.now.com/home/local", "XPATH_LINK":"//a[@class='newsWrap clearfix']", "XPATH_TITLE":"//div[@class='newsTitle']"}]
-
-search_by_two_XPATH(myList_2)
-
-
-
-def TVB():
-    driver = webdriver.Chrome(r"C:\Users\user\PycharmProjects\web_scraping\chromedriver")
-    driver.get("https://news.tvb.com/tc/local")
-    driver.implicitly_wait(25)
-    elem = driver.find_elements(By.TAG_NAME, "a") + driver.find_elements(By.XPATH, "//div[@class='entryContent']/h3")
-    for i in elem:
-        print(i.text)
-        print(i.get_attribute("href"))
-    driver.quit()
